abe963eb3e8e6ad3d1a4ca9aecff4d338ca13892
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-03-22 12:50:24 -0400

    exercise 1 is done

---
+++ shapes.py
@@ -0,0 +1,18 @@
+
+class rectangle():
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+    def perimeter(self):
+        return (2 * self.length) + (2 * self.width)
+    
+class square(rectangle):
+    def __init__(self, side):
+        self.length = side
+        self.width = side
+    pass
+
---
+++ shapes_tester.py
@@ -0,0 +1,8 @@
+import shapes
+
+rect = shapes.rectangle(2,4)
+print(rect.area())
+
+square = shapes.square(8)
+print(square.area())
+print(square.perimeter())
