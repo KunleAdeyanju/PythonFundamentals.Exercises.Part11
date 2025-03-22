51804b77fdbedcf88f22641efe70a36345c28fe5
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-03-22 14:13:16 -0400

    making progress, creating classroom class

--- gradebook.py
+++ gradebook.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import uuid
 
 class Alive_status(Enum):
     alive = 1
@@ -24,7 +25,26 @@ class Person():
         self.status = status
 
 
+class Instructor(Person):
+    def __init__(self, first_name, last_name, dob, status: Alive_status):
+        super().__init__(first_name, last_name, dob, status: Alive_status)
+        self.instructor_id = "Instructor_" + str(uuid.uuid4())
 
+class Student(Person):
+    def __init__(self, first_name, last_name, dob, status: Alive_status):
+        super().__init__(first_name, last_name, dob, status: Alive_status)
+        self.student_id = "Student_" + str(uuid.uuid4())
+
+class ZipCodeStudent(Student):
+    pass
+
+class StupperStudents(Student):
+    pass
+
+class Classroom():
+    def __init__(self):
+        pass
         
+
         
        
